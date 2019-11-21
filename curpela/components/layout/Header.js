15160c5d808046b36f711afa67bfdf90ef615582
Anthony Venturini <anthonydventurini@gmail.com>
2019-11-21 11:50:09 -0800

    adds header styles - working on getting background image to show

--- curpela/components/layout/Header.js
+++ curpela/components/layout/Header.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Link from "next/link";
-import './Header.scss'
-
+import Nav from "./Nav";
+import "./Header.scss";
 
 const Header = () => (
-  <div className='header'>
-      <h1>Welcome to Curpela</h1>
-      <h2>Homes Live Here</h2>
-      <button>Discover</button>
+  <div className="header">
+    <Nav />
+    <h1>Welcome to Curpela</h1>
+    <h2>Homes Live Here</h2>
+    <button>Discover</button>
   </div>
 );
 
--- curpela/pages/index.js
+++ curpela/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Head from 'next/head'
-import Nav from '../components/layout/Nav'
 import Header from '../components/layout/Header'
 import "../styles/base.scss";
 
@@ -10,8 +9,6 @@ const Home = () => (
       <title>curpela | Homes Live Here</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-
-    <Nav />
     <Header />
 
   </div>
