9571a2aeeffb7fcda9659e6f0df4afbf9d749e7a
Anthony Venturini <anthonydventurini@gmail.com>
2019-11-24 21:28:43 -0800

    Adds styling for login

--- curpela/components/authentication/Login.js
+++ curpela/components/authentication/Login.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import './Login.scss'
+import React from "react";
+import "./Login.scss";
 
 export default function Login() {
-    return (
-        <div className="login-container">
-            
-        </div>
-    )
+  return (
+    <div className="login-container">
+      <div className="login-content-left">
+        <div className="logo"></div>
+        <h2>Curpela Has the <br /> Best Houses</h2>
+      </div>
+      <div className="login-content-right">
+          <div className="login-content-form">
+            <div className="login-form-type">
+          <h3>Sign In</h3>
+          <p>New User? Create an Account</p>
+          </div>
+          <form>
+          <input className="username" placeholder="Username"></input>
+          <input placeholder="Password"></input>
+          <button>Log In</button>
+          </form>
+          <p>or</p>
+          <button className="Google">Sign in with Google</button>
+          <button className="Facebook">Sign in with Facebook</button>
+      </div>
+      </div>
+    </div>
+  );
 }
--- curpela/components/layout/Header.js
+++ curpela/components/layout/Header.js
@@ -8,7 +8,7 @@ const Header = () => (
   <div className="header">
     <div className="header-content">
     <h1>Built to sell real estate</h1> 
-    <h2>Powered by the content creators</h2>
+    <h2>Powered by content creators</h2>
     {/* <input type="text" placeholder="Enter a city or Zip code"></input> */}
     <button>Explore the Platform</button>
     </div>
