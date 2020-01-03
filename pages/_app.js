264aad62b69fb05579c4b7cfd291046c6fe3b5d5
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-02 21:41:58 -0800

    Create _app component
    Created _app component to wrap all components with apolo

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import React from "react";
+import App from "next/app";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { withApollo } from "../config/apollo.config";
+
+import "../styles/index.scss";
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps, apollo } = this.props;
+    return (
+      <ApolloProvider client={apollo}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    );
+  }
+}
+
+// Wraps all components in the tree with the data provider
+export default withApollo(MyApp);
