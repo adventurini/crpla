c9b796b7292b3113395824ac27350ebe68fccd98
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-02 21:43:52 -0800

    Remove styles from app

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,6 @@ import App from "next/app";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { withApollo } from "../config/apollo.config";
 
-import "../styles/index.scss";
-
 class MyApp extends App {
   render() {
     const { Component, pageProps, apollo } = this.props;
