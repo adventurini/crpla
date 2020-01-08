1779764cbf5c2a63f9a571bbb92ae72717266897
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-08 01:34:17 -0800

    Formatting

--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import Login from '../components/authentication/Login';
+import React from "react";
+import Login from "../components/authentication/Login";
 import redirect from "../utils/redirect";
 import nextCookie from "next-cookies";
-import Nav from '../components/layout/Nav'
+import Nav from "../components/layout/Nav";
 
 function login() {
-    return (
-        <div className="login-container">
-            <Nav />
-            <Login />
-        </div>
-    )
+  return (
+    <div className="login-container">
+      <Nav />
+      <Login />
+    </div>
+  );
 }
-    Login.getInitialProps = ctx => {
-        const auth = nextCookie(ctx).token;
-        if (auth) redirect(ctx, "/");
-        return {};
-      };
+Login.getInitialProps = ctx => {
+  const auth = nextCookie(ctx).token;
+  if (auth) redirect(ctx, "/");
+  return {};
+};
 
-export default login
+export default login;
