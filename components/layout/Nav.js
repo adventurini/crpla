c225d44c69d711b2cc2b1446f54f7d082a7edf28
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-08 01:33:24 -0800

    Replace text logo with component logo

--- components/layout/Nav.js
+++ components/layout/Nav.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import CurpelaWordLogo from "../icons/CurpelaWordLogo";
 import "./Nav.scss";
 
 const links = [
@@ -11,34 +12,35 @@ const links = [
   return link;
 });
 
-const Nav = () => (
-  <div className="nav">
-    <div className="left-nav">
-      <Link href="/">
-        <a>curpela</a>
-      </Link>
-    </div>
-    <div className="center-nav">
-      <ul>
-        {links.map(({ key, href, label }) => (
-          <li key={key}>
-            <Link href={href}>
-            <a>{label}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+const Nav = () => {
+  return (
+    <div className="nav">
+      <div className="left-nav">
+        <Link href="/">
+          <CurpelaWordLogo fill="#483bda" width="120" />
+        </Link>
+      </div>
+      <div className="center-nav">
+        <ul>
+          {links.map(({ key, href, label }) => (
+            <li key={key}>
+              <Link href={href}>
+                <a>{label}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-    <div className="right-nav">
-      <a href="/login">
-        <button>Log In</button>
-      </a>
-      <a href="/signup">
-        <button className="signup">Sign Up</button>
-      </a>
+      <div className="right-nav">
+        <a href="/login">
+          <button>Log In</button>
+        </a>
+        <a href="/signup">
+          <button className="signup">Sign Up</button>
+        </a>
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default Nav;
