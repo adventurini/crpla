9b581444216f2a8660f55ce75fd7f799b6a188b5
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-01-08 01:34:07 -0800

    Add dynamic rendering for feed

--- pages/index.js
+++ pages/index.js
@@ -3,47 +3,60 @@ import Head from "next/head";
 import Header from "../components/layout/Header";
 import Nav from "../components/layout/Nav";
 import FeaturedHouses from "../components/landingPage/FeaturedHouses";
-import FeaturedPhotographers from "../components/landingPage/FeaturedPhotographers";import 
-FeaturedHashtags from "../components/landingPage/FeaturedHashtags";
+import FeaturedPhotographers from "../components/landingPage/FeaturedPhotographers";
+import FeaturedHashtags from "../components/landingPage/FeaturedHashtags";
 import Footer from "../components/layout/Footer";
 import "../styles/base.scss";
 
-const Home = () => (
-  <div>
-    <Head>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      ></link>
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      ></link>
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      ></link>
-      <link rel="manifest" href="/site.webmanifest"></link>
-      <link
-        rel="mask-icon"
-        href="/safari-pinned-tab.svg"
-        color="#0e1977"
-      ></link>
-      <meta name="msapplication-TileColor" content="#0e1977"></meta>
-      <meta name="theme-color" content="#0e1977"></meta>
-    </Head>
-    <Nav />
-    <Header />
-    {/* <FeaturedHouses />
+import NavAuth from "../components/layout/NavAuth";
+import UserFeed from "../components/feeds/UserFeed";
+import nextCookie from "next-cookies";
+
+const Home = ({ isAuthenticated }) => {
+  return (
+    <div>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg"
+          color="#0e1977"
+        ></link>
+        <meta name="msapplication-TileColor" content="#0e1977"></meta>
+        <meta name="theme-color" content="#0e1977"></meta>
+      </Head>
+
+      {isAuthenticated ? <NavAuth /> : <Nav />}
+      {isAuthenticated ? <UserFeed /> : <Header />}
+
+      {/* <FeaturedHouses />
     <FeaturedPhotographers />
     <FeaturedHashtags />
     <Footer /> */}
-  </div>
-);
+    </div>
+  );
+};
+
+Home.getInitialProps = ctx => {
+  const auth = nextCookie(ctx).token;
+  return { isAuthenticated: auth ? auth : false };
+};
 
 export default Home;
