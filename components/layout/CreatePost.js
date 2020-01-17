e10f40f33c0c85f792e7fbc6434fe329f7eb2586
Anthony Venturini <anthonydventurini@gmail.com>
2020-01-17 13:23:19 -0800

    Makes dynamic preview for image thumbnails on Create Post & adds max width constraint to news feed

--- components/layout/CreatePost.js
+++ components/layout/CreatePost.js
@@ -1,47 +1,96 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./CreatePost.scss";
 import UserAvatar from "./UserAvatar";
 import Button from "./Button";
-import {FaRegImage, FaHome, FaRegSmile, FaHashtag} from "react-icons/fa";
-import {GoMention} from "react-icons/go";
-import FileUploader from './FileUploader';
-
+import { FaRegImage, FaHome, FaRegSmile, FaHashtag } from "react-icons/fa";
+import { GoMention } from "react-icons/go";
+import FileUploader from "./FileUploader";
+// import Picker, {SKIN_TONE_MEDIUM_DARK} from 'emoji-picker-react'
 
 export default function CreatePost() {
-
   const [preview, setPreview] = useState([]);
   const [post, setPost] = useState({
-    text: '',
+    text: "",
     media: []
-  })
+  });
 
+  // const [chosenEmoji, setChosenEmoji] = useState(null);
 
-    const image= "https://ca.slack-edge.com/T4JUEB3ME-UF0MEJPQS-456f70806ec0-512"
+  //   const onEmojiClick = (event, emojiObject) => {
+  //       setChosenEmoji(emojiObject);
+  //   }
+
+  // const fireEmojis = () => {
+  //   <Picker onEmojiClick={onEmojiClick} skinTone={SKIN_TONE_MEDIUM_DARK}/>
+  // }
+
+  const image =
+    "https://ca.slack-edge.com/T4JUEB3ME-UF0MEJPQS-456f70806ec0-512";
 
   return (
-    <FileUploader noClick post={post} setPost={setPost} preview={preview} setPreview={setPreview}>
-    <div className="create-post">
-      <div className="create-post-top">
-        <div className="create-post-user-avatar"><UserAvatar image={image} heigh="70px" width="70px"/></div>
-        <input placeholder="Share something new..."></input>
-      </div>
-      <div className={`create-post-image-preview`}>
-    {preview.length > 0 && preview.map(preview=> <img src={preview.preview} />)}
-      </div>
-      <div className="create-post-bottom">
-          <div className="create-post-icons">
-            <FileUploader post={post} setPost={setPost} preview={preview} setPreview={setPreview}>
-        <FaRegImage className="create-post-icon image-icon"/>
-        </FileUploader>
-      <FaRegSmile className="create-post-icon smile-icon" />
-      <GoMention className="create-post-icon mention" />
-      <FaHome className="create-post-icon home" />
-
-        
+    <FileUploader
+      noClick
+      post={post}
+      setPost={setPost}
+      preview={preview}
+      setPreview={setPreview}
+    >
+      <div className="create-post">
+        <div className="create-post-top">
+          <div className="create-post-user-avatar">
+            <UserAvatar image={image} heigh="70px" width="70px" />
           </div>
-          <div className="create-post-button"><Button text="Create Post"/></div>
+          <input placeholder="Share something new..."></input>
+        </div>
+
+        <div className="create-post-bottom">
+          <div className="create-post-icons">
+            <FileUploader
+              post={post}
+              setPost={setPost}
+              preview={preview}
+              setPreview={setPreview}
+            >
+              <FaRegImage className="create-post-icon image-icon" />
+            </FileUploader>
+            <FaRegSmile className="create-post-icon smile-icon" />
+            <GoMention className="create-post-icon mention" />
+            <FaHome className="create-post-icon home" />
+          </div>
+          <div className="create-post-button">
+            <Button text="Create Post" />
+          </div>
+        </div>
+        <div
+          className={`create-post-preview-image-container-hidden ${preview.length >
+            0 && "create-post-preview-image-container-show"}`}
+        >
+          {preview.length > 0 &&
+            preview.map((image, i) => (
+              <>
+                {preview.length === 1 ? (
+                  <img
+                    className="create-post-preview-single-image"
+                    key={i}
+                    src={image.preview}
+                  />
+                ) : preview.length === 2 ? (
+                  <img
+                    className="create-post-preview-double-image"
+                    key={i}
+                    src={image.preview}
+                  />
+                ) : (
+                  <img
+                    className="create-post-preview-image"
+                    key={i}
+                    src={image.preview}
+                  />
+                )}
+              </>
+            ))}
+        </div>
       </div>
-    </div>
     </FileUploader>
   );
 }
