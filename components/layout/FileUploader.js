39483d8991d5c61657b70980809355c5803e6efb
Anthony Venturini <anthonydventurini@gmail.com>
2020-01-23 19:23:05 -0800

    Add preview to drag and drop listing generator component

--- components/layout/FileUploader.js
+++ components/layout/FileUploader.js
@@ -1,36 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useDropzone} from 'react-dropzone';
 
-const thumbsContainer = {
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap'
-};
-
-const thumb = {
-  display: 'inline-flex',
-  borderRadius: 2,
-  border: '1px solid #eaeaea',
-  marginBottom: 8,
-  marginRight: 8,
-  width: 100,
-  height: 100,
-  padding: 4,
-  boxSizing: 'border-box'
-};
-
-const thumbInner = {
-  display: 'flex',
-  minWidth: 0,
-  overflow: 'hidden'
-};
-
-const img = {
-  display: 'block',
-  width: 'auto',
-  height: '100%'
-};
-
 
 function FileUploader({children, noClick, setThumbnail, preview, setPreview, post, setPost}) {
 
--- components/photoUploader/PhotoUpload.js
+++ components/photoUploader/PhotoUpload.js
@@ -1,30 +1,60 @@
-import React, {useState} from 'react';
-import './PhotoUpload.scss';
-import {MdCloudUpload} from 'react-icons/md'
+import React, { useState } from "react";
+import "./PhotoUpload.scss";
+import { MdCloudUpload } from "react-icons/md";
+import FileUploader from "../../components/layout/FileUploader";
 
 export default function PhotoUpload() {
+  const [preview, setPreview] = useState([]);
+  const [post, setPost] = useState({
+    text: "",
+    media: []
+  });
 
-    const [preview, setPreview] = useState([])
 
-    return (
-        <div className="photo-upload">
-            <h1 className="photo-upload-title">Add Listing Photos</h1>
-            <div className="photo-upload-dnd-container">
-                <MdCloudUpload className='photo-upload-icon'/>
-                <div className="photo-upload-content-container">
-                    <p className="photo-upload-content">Drag & Drop files here <br /> or <br /> Click <span className ="underline">here</span> to upload </p>
-                </div>
-            </div>
-            <div className="photo-upload-preview-container">
-
-                {preview.length > 0 &&
-
-                preview.map(image => {
-                    <img src={image} className="photo-upload-preview-image"></img>
-                })
-            
-}
-            </div>
+  return (
+    <div className="photo-upload">
+      <FileUploader noClick preview={preview} setPreview={setPreview} post={post} setPost={setPost}>
+        <h1 className="photo-upload-title">Add Listing Photos</h1>
+        <div className="photo-upload-dnd-container">
+          <MdCloudUpload className="photo-upload-icon" />
+          <FileUploader preview={preview} post={post} setPost={setPost} setPreview={setPreview}>
+          <div className="photo-upload-content-container">
+            <p className="photo-upload-content">
+              Drag & Drop files here <br /> or <br /> Click{" "}
+              
+                <span className="underline">here</span>
+             {" "}
+              to upload{" "}
+            </p>
+          </div>
+          </FileUploader>
         </div>
-    )
+      </FileUploader>
+      <div className={`photo-upload-preview-image-container-hidden ${preview.length >
+            0 && "photo-upload-preview-image-container-show"}`}>
+               {preview.length > 0 &&
+            preview.map((image, i) => (
+              <>
+                {i===0 ? (
+                    <>
+                <h2 className="photo-upload-preview-image-heading">Header Image</h2>
+                  <img
+                    className="photo-upload-preview-image-header"
+                    key={i}
+                    src={image.preview}
+                  />
+                  </>
+                ) : (
+                    
+                  <img
+                    className="photo-upload-preview-image"
+                    key={i}
+                    src={image.preview}
+                  />
+                ) }
+              </>
+            ))}
+      </div>
+    </div>
+  );
 }
