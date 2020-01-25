54ac48cfe62b4e825e123a8a5c440af887dc2833
Anthony Venturini <anthonydventurini@gmail.com>
2020-01-25 14:45:17 -0800

    Implements react-sortable-hoc for image previewing in listing generator:

--- components/layout/FileUploader.js
+++ components/layout/FileUploader.js
@@ -3,22 +3,25 @@ import {useDropzone} from 'react-dropzone';
 
 
 function FileUploader({children, noClick, setThumbnail, preview, setPreview, post, setPost}) {
-
+  console.log(preview)
   const {getRootProps, getInputProps} = useDropzone({
     accept: 'image/*',
     onDrop: acceptedFiles => {
+      console.log(1)
       setPost({
         ...post, 
         media: [...post.media, ...acceptedFiles]
       })
-     
+
       setPreview([...preview, ...acceptedFiles.map(file => Object.assign(file, {
         preview: URL.createObjectURL(file)
       }))]);
+      
     },
+
     noClick : noClick ? true : false
   });
-  
+  console.log(preview)
 
   // useEffect(() => () => {
   //   // Make sure to revoke the data uris to avoid memory leaks
@@ -27,6 +30,7 @@ function FileUploader({children, noClick, setThumbnail, preview, setPreview, pos
   
   return (
     <section className="container">
+      {console.log(preview)}
       <div {...getRootProps({className: 'dropzone'})}>
         <input {...getInputProps()} />
         {children}
--- components/photoUploader/PhotoUpload.js
+++ components/photoUploader/PhotoUpload.js
@@ -2,8 +2,49 @@ import React, { useState } from "react";
 import "./PhotoUpload.scss";
 import { MdCloudUpload } from "react-icons/md";
 import FileUploader from "../../components/layout/FileUploader";
-import ImagePreviewModal from '../postCard/ImagePreviewModal'
-import CurpelaWordLogo from '../../components/icons/CurpelaWordLogo'
+import ImagePreviewModal from "../postCard/ImagePreviewModal";
+import CurpelaWordLogo from "../../components/icons/CurpelaWordLogo";
+import { sortableContainer, sortableElement } from "react-sortable-hoc";
+import arrayMove from "array-move";
+
+const SortableImagessContainer = sortableContainer(({ children, preview }) => (
+  <div
+    className={`photo-upload-preview-image-container-hidden ${preview.length >
+      0 && "photo-upload-preview-image-container-show"}`}
+  >{console.log("container")}
+    {children}
+  </div>
+));
+
+const SortableImage = sortableElement((props) => {
+  
+  const {image, fireModal, index} = props
+  return(
+  <img
+      onClick={() => fireModal(index)}
+      className="photo-upload-preview-image"
+      key={index}
+      src={image.preview}
+    />)
+    
+  // return(
+  // imageIndex === 0 ? (
+      
+  //     <img
+  //       onClick={() => fireModal(imageIndex)}
+  //       className="photo-upload-preview-image-header"
+  //       key={imageIndex}
+  //       src={image.preview}
+  //     />
+  // ) : (
+  //   <img
+  //     onClick={() => fireModal(imageIndex)}
+  //     className="photo-upload-preview-image"
+  //     key={imageIndex}
+  //     src={image.preview}
+  //   />
+  // ))
+});
 
 export default function PhotoUpload() {
   const [preview, setPreview] = useState([]);
@@ -11,92 +52,99 @@ export default function PhotoUpload() {
     text: "",
     media: []
   });
-
-  const [step, setStep] = useState(0)
-
-  const fireModal = (i) => {
-      setImageModal(true);
-      setIndex(i)
-  }
-
+  const [step, setStep] = useState(0);
   const [imageModal, setImageModal] = useState(false);
-  const [index, setIndex] = useState(0)
+  const [modalIndex, setModalIndex] = useState(0);
 
+
+  
+  const fireModal = index => {
+    setImageModal(true);
+    setModalIndex(index);
+  };
+  
+  const onSortEnd = ({ oldIndex, newIndex }) =>
+    setPreview(arrayMove(preview, oldIndex, newIndex));
 
   return (
-      <div className="photo-upload">
-          {step === 0 ?
-    <div className="photo-upload-container">
-        
-        <div className="photo-upload-content-container">
-        
-        
-            <>
-        <div className="photo-upload-logo">
-         <CurpelaWordLogo fill="#483bda" width="240"/>
-         </div>
-        <form className="photo-upload-address-form" onSubmit={()=>setStep(1)}>
-            
-        <input className="photo-upload-address-input" placeholder="Enter Address"></input>
-        <button className="photo-upload-address-submit">Next</button>
-        </form>
-        <p className="photo-upload-explainer">Welcome to our listing content generator. Please enter the listing address to continue.</p>
-        </>
-        </div>
-        </div>
-        :
-       
-      <FileUploader noClick preview={preview} setPreview={setPreview} post={post} setPost={setPost}>
-        <h1 className="photo-upload-title">Add Listing Photos</h1>
-        <div className="photo-upload-dnd-container">
-          <MdCloudUpload className="photo-upload-icon" />
-          <FileUploader preview={preview} post={post} setPost={setPost} setPreview={setPreview}>
+    <div className="photo-upload">
+      {step === 0 ? (
+        <div className="photo-upload-container">
           <div className="photo-upload-content-container">
-            <p className="photo-upload-content">
-              Drag & Drop photos here <br /> or <br /> Click{" "}
-              
-                <span className="underline">here</span>
-             {" "}
-              to upload{" "}
-            </p>
+            <>
+              <div className="photo-upload-logo">
+                <CurpelaWordLogo fill="#483bda" width="240" />
+              </div>
+              <form
+                className="photo-upload-address-form"
+                onSubmit={() => setStep(1)}
+              >
+                <input
+                  className="photo-upload-address-input"
+                  placeholder="Enter Address"
+                ></input>
+                <button className="photo-upload-address-submit">Next</button>
+              </form>
+              <p className="photo-upload-explainer">
+                Welcome to our listing content generator. Please enter the
+                listing address to continue.
+              </p>
+            </>
           </div>
-          </FileUploader>
         </div>
-      </FileUploader>
+      ) : (
+        <>
 
-}
-      <div className={`photo-upload-preview-image-container-hidden ${preview.length >
-            0 && "photo-upload-preview-image-container-show"}`}>
-               {preview.length > 0 &&
-            preview.map((image, i) => (
-              <>
-                {i===0 ? (
-                    <>
-                <h2 className="photo-upload-preview-image-heading">Header Image</h2>
-                  <img
-                  onClick={()=> fireModal(i)}
-                    className="photo-upload-preview-image-header"
-                    key={i}
-                    src={image.preview}
-                  />
-                  </>
-                ) : (
-                    
-                  <img
-                  onClick={()=> fireModal(i)}
-                    className="photo-upload-preview-image"
-                    key={i}
-                    src={image.preview}
-                  />
-                ) }
-              </>
-            ))}
-          
-      </div>
+        <FileUploader
+          noClick
+          preview={preview}
+          setPreview={setPreview}
+          post={post}
+          setPost={setPost}
+        >
+          <div className="photo-upload-dnd">
+            <h1 className="photo-upload-title">Add Listing Photos</h1>
+            <div className="photo-upload-dnd-container">
+              <MdCloudUpload className="photo-upload-icon" />
+              <FileUploader
+                preview={preview}
+                post={post}
+                setPost={setPost}
+                setPreview={setPreview}
+              >
+                <div className="photo-upload-content-container">
+                  <p className="photo-upload-content">
+                    Drag & Drop photos here <br /> or <br /> Click{" "}
+                    <span className="underline">here</span> to upload{" "}
+                  </p>
+                </div>
+              </FileUploader>
+            </div>
+          </div>
+        </FileUploader>
+        </>
+      )}
 
-      {imageModal && <ImagePreviewModal setPreview={setPreview} preview={preview} index={index} setImageModal={setImageModal} />}
+        {preview.length > 0 &&
+      <SortableImagessContainer distance={1} axis="xy" onSortEnd={onSortEnd} preview={preview}>
+        <h2 className="photo-upload-preview-image-heading">Header Image</h2>
+         {preview.map((image, index) => {
+                return(
+            <SortableImage image={image} fireModal={()=>fireModal(index)} index={index} key={index}/>
+            )})}
+      </SortableImagessContainer>
+          }
+
+      {imageModal && (
+        <ImagePreviewModal
+          setPreview={setPreview}
+          preview={preview}
+          index={modalIndex}
+          setIndex={setModalIndex}
+          setImageModal={setImageModal}
+        />
+      )}
       {imageModal && <div className="photo-upload-modal"></div>}
-      
     </div>
   );
 }
